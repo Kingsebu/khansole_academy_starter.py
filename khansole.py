92f802bdee67d8d7a47450a77c448f03eb585095
Kingsebu <82843235+Kingsebu@users.noreply.github.com>
2021-04-20 00:03:48 +0000

    Update khansole.py

--- khansole.py
+++ khansole.py
@@ -1,9 +1,19 @@
+#importing the random package into python
+import random
+
+#setting out to count attempts
 count = 0
+
+#as long as count is less than three input from user should continue
 while count < 3:
 
+#generating the two random numbers between 10 and 99 and adding them up.
     num1 = random.randint(10, 99)
     num2 = random.randint(10, 99)
     sum = num1 + num2
+
+#recieving input from user. Where 3 right responses are entered in a row the program ends with congratulatory message
+#where a wrong answer is provided, the program will continue.
     response = int(input(f'What is {num1} + {num2}? '))
     if response == sum:
         count += 1
